10d55d01c84c1153cb179b3f5beb3a18ac7d6137
Polyanetuag <pollyportugues@gmail.com>
2024-07-30 17:35:10 -0300

    feat: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
-  return <h1>TESTE</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h1>Não encontrado</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
