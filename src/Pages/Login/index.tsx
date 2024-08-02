ba43b1a6c5e4a629afe7a29a2be3ece1201a26f5
Polyanetuag <pollyportugues@gmail.com>
2024-08-02 17:56:07 -0300

    feat: login page sketch

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <div style={{ background: "black" }}>
+      <h1>Login</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
